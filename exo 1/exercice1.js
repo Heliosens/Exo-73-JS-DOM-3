031546a4a16eabcee735b46e712ddab44ca24c6b
Sylvie-B <unsolo@hotmail.fr>
2021-11-18 10:20:40 +0100

    affecter alt aux title

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,12 +1,7 @@
 let avatarEls = document.getElementsByTagName("img");
 
-
-
-
-
-
-
-
-
-
+for(let i = 0 ; i < avatarEls.length ; i++){
+    avatarEls[i].style.height = '100px';
+    avatarEls[i].title = avatarEls[i].alt;
+}
 
