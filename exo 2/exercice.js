0fcba3d459059d970ee8a802a1ccca250b18e980
Sylvie-B <unsolo@hotmail.fr>
2021-11-18 10:29:37 +0100

    affecter text or border

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -2,8 +2,6 @@ let colorEl = document.getElementById("trendy-color");
 let handEl  = document.getElementById("trendy-handwriting");
 let frameEl = document.getElementById("trendy-frame");
 
-
-
-
-
-
+colorEl.innerHTML = "blue";
+handEl.innerHTML = "X";
+frameEl.style.border = "10px groove orange";
